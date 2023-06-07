0b1d932555199ea9d836bf11eb637d8b0ced7a2c
Vitalii Kononenko <vk@computools.com>
2023-06-07 16:51:40 +0300

    Boilerplate

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function Dashboard() {
+  return (
+    <div><h1>Im a dashboard page!</h1></div>
+    )
+}
---
+++ app/my-profile/page.tsx
@@ -0,0 +1,6 @@
+export default function MyProfile() {
+  return (
+    <div><h1>Im a profile page!</h1></div>
+  )
+}
+  
